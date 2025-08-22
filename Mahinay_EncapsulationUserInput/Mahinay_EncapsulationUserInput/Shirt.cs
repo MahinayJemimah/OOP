5814ac8b05f7ab13ca5a53776110f5cb622bfef6
ACT-STUDENT <ACT-STUDENT@LAB3-PC40>
2025-08-22 16:32:29 +0800

    EncapsulationUserInput

---
+++ Mahinay_EncapsulationUserInput/Mahinay_EncapsulationUserInput/Shirt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahinay_EncapsulationUserInput
+{
+    internal class Shirt
+    {
+        // Declaring private fields
+        public string Brand { get; set; }
+        public string Size { get; set; } 
+
+        // Method to display details
+        public void DisplayDetails()
+        {
+            Console.WriteLine( "    Shirt Details:" + "\n" +
+                              "Shirt Brand: " + Brand + "\n" +
+                              "Shirt Size : " + Size);
+        }
+    }
+}
